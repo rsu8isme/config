512fd9a6769bc4e92ab2f98e824b15a086e0542d
rsu8 <rsu8isme@gmail.com>
2024-03-01 00:13:23 +0700

    Init config files

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -gx COPILOT_API_KEY copilot_key
+    set -gx HANDLER copilot
+    set -gx HELIX_RUNTIME /home/rsu8/AppsBuiltFromSrc/helix/runtime
+end
